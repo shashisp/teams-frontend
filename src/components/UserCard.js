73205fa83dd7a698f0460817fe9f76d86c706e14
Shashi Kumar P <shashiessp@gmail.com>
2019-11-06 19:16:30 -0600

    remove unused UI components and fix link and prevent onclick refreshing

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,32 +1,27 @@
-import React from 'react'
-import { Divider, Header, Image, Segment } from 'semantic-ui-react'
+import React from "react";
+import { Header } from "semantic-ui-react";
 import { Link } from "react-router-dom";
-import faker from 'faker';
+import faker from "faker";
 
-const UserCard = (props) => (
+const UserCard = props => (
+  <Link to={`user/edit/${props.user.id}`}>
     <div className="ui raised very padded segment">
-        <a href="" className="avatar">
-            <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-            <Link to={`user/edit/${props.user.id}`} >
-                <Header as='p' floated='right'>
-                    {props.user.firstName} {props.user.lastName} {props.user.userType === 'admin' ? '(admin)' : ''}
-                </Header>
-            </Link>
+      <a href="" className="avatar">
+        <img alt="avatar" src={faker.image.avatar()} />
+      </a>
+      <div className="content">
+        <Header as="p" floated="right">
+          {props.user.firstName} {props.user.lastName}{" "}
+          {props.user.userType === "admin" ? "(admin)" : ""}
+        </Header>
 
-            <div className="metadata">
-                <span className="phone">
-                    {props.user.phone}
-                </span>
-                <span className="email">
-                    {props.user.email}
-                </span>
-
-            </div>
-
+        <div className="metadata">
+          <span className="phone">{props.user.phone}</span>
+          <span className="email">{props.user.email}</span>
         </div>
+      </div>
     </div>
-)
+  </Link>
+);
 
 export default UserCard;
